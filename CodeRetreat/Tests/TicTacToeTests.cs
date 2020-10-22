46343f6461fb6610603485416fd223b36169c7d9
wouter.duckaert <wouter.duckaert@protime.eu>
2020-10-22 10:58:35 +0200

    Add first test

---
+++ CodeRetreat/Tests/TicTacToeTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    class TicTacToeTests
+    {
+        [Test]
+        public void GameStarted_ShowsEmptyBoard()
+        {
+            var result = "   |   |   ";
+            Assert.That(result, Is.EqualTo("   |   |   "));
+        }
+    }
+}
